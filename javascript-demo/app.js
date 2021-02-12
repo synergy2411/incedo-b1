8de538a06f5ebcc885eae16b8d405e7f2a3c9365
unknown <Sumit Khandelwal>
2021-02-12 10:31:00 +0530

    new code - day 02

--- javascript-demo/app.js
+++ javascript-demo/app.js
@@ -15,31 +15,113 @@
 
 // console.log("Finishing the program");
 
-// FUNCTIONS
-//  - are values
+// FUNCTIONS - first class citizens
+//  - are not just syntax, but are values
 //  - can be anonymous
 //  - can be nested to other functions
 //  - can be returned 
 //  - can be supplied as parameter to other functions (Callback)
+//  - IIFE (immediately invoked function expression)
+//  - restricts the scope of variables
+//  - Closure - is the process of accessing the outer scope variables in nested functions
+
+
+// function buildTicket(transport){
+//     var numOfPassengers = 0;
+
+//     var nestedFunc = function(name){
+//         return "Hello " + name + "\nYour ticket ID is #"+ (++numOfPassengers)
+//             + "\nYou are going via transport " + transport;
+//     }
+//     return nestedFunc;
+// }
+
+// // var shipFunc = buildTicket('Ship');
+// console.log(buildTicket('Ship')('Foo'));
+// console.log(buildTicket('Ship')('Bar'));
+// // console.log(shipFunc("Foo"));
+// // console.log(shipFunc("Bar"));
+
+// var carFunc = buildTicket('Car');
+// console.log(carFunc("Bam"));        
+// // console.log(shipFunc("John"));        
+
+
+
+
+
+
+// function testClosure(){
+//     var x = 100;
+//     function nestedFunc(){
+//         console.log(x);
+//     };
+//     retrun nestedFunc;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// ES5 feature
+// IIFE - self invoked
+// (function(){
+//     console.log("IIFE works");
+//     // restrict the code 
+// })()
+
+// Global variable
+
+// var x = 101;
+
+// function demo(){
+//     // Local variable
+//     var x = 201;
+//     x++;
+//     console.log(x);
+// }
+
+// console.log("Outside function - ",  x);         // ?
+
+// demo();         // ?
+
+
+// index.html
+//  - app.js - var x = 100;
+//  - main.js - x =100;
+
+
+
+
+
+
+
+
 
 // ERROR FIRST, CALLBACK LAST
-function greet(err, message){
-    if(err){
-        console.log(err);
-        process.exit(1);
-    }
-    console.log(message);
-}
+// function greet(err, message){
+//     if(err){
+//         console.log(err);
+//         process.exit(1);
+//     }
+//     console.log(message);
+// }
 
-function greetEnglish(arr, cb){
-    if(arr.length > 3){
-        cb(null, "SUCCESS");
-    }else{
-        cb(new Error("Something Bad happened..."));
-    }
-}
+// function greetEnglish(arr, cb){
+//     if(arr.length > 3){
+//         cb(null, "SUCCESS");
+//     }else{
+//         cb(new Error("Something Bad happened..."));
+//     }
+// }
 
-greetEnglish( [1,2,3,4,5], greet );
+// greetEnglish( [1,2,3,4,5], greet );
 
 
 
@@ -102,4 +184,87 @@ greetEnglish( [1,2,3,4,5], greet );
 
 // console.log(add(2,4));
 // console.log(addition(5,5));
-// console.log(addition());
+// console.log(addition(3,4));
+
+
+
+
+
+// Object - JS (ES5) is Object based / ES6 is Object oriented - Classes, extends, interface, 
+
+//  - Function
+//  - Array
+//  - Object
+//  - String 
+//  - Number
+//  - Date
+
+// 3 ways to create an Object
+
+//  - Literal
+
+    // var myFriends = ["Bar", "Bam","Baz"];
+
+    // var user = {
+    //     name  : "Foo",
+    //     address : "201, Main Street",
+    //     age : 32,
+    //     friends : myFriends,
+    //     getDetails : function(){
+    //         return this.name + " " + this.address;
+    //     },
+    //     isAdmin : true,
+    //     addNewFriend : function(newFriend){
+    //         this.friends.push(newFriend);
+    //     }
+    // };
+
+    // user.addNewFriend("Pqr");
+    // console.log(user.friends.length);       // 4
+    // console.log(myFriends.length);          // 4
+
+    // Reference Types - Objects, Arrays, Functions
+    // Primitive Types - String, Number, Date etc
+
+    // var x = [1,2,3];
+    // var y = x;
+    // y.push(4);
+    // console.log(x);
+
+
+    // var user = {
+    //     name : "Foo"
+    // }
+
+    // var user2 = user;
+    // user2.name = "Bar";
+
+    // console.log(user2.name);            // ?
+    // console.log(user.name);            // ?
+
+
+    // user = X019284X - memory address
+    // var user = {
+    //     name : "Foo"
+    // }
+
+    // // 1 -// user = X019284X - memory address
+    // user.name = "Bar";
+    
+    // // 2 - // user = X0374543 - new memory address
+    // user = {
+    //     name : "Baz"
+    // }
+
+
+
+
+
+
+
+
+
+
+
+//  - Contructor
+//  - Instance 
