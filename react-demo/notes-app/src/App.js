058c3a1124ec82698758dfe4db0bf45abf016d33
unknown <Sumit Khandelwal>
2021-02-16 11:37:50 +0530

    Notes App structure created

--- react-demo/notes-app/src/App.js
+++ react-demo/notes-app/src/App.js
@@ -1,38 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ClassBasedCompDemo from './components/Types/ClassBasedCompDemo';
-
-// import the named function
-// import {FunctionBasedCompDemo, a, b} from "./components/Types/FunctionBasedCompDemo";
-
-// import the default function
-// import xyz, {a} from "./components/Types/FunctionBasedCompDemo";
-// import Parent from './components/CmpCommunication/Parent';
-
-import Parent from './components/StateDemo/Parent';
 
 function App() {
   return (
-    // React.createElement("div",{className : "App"}, 
-    //   React.createElement("p",{}, "Hello World again!"))
-    <div className="App">
-      <p className="display-4"> Hello World!</p>
-
-      <Parent />
-
-      {/* <hr />
-      <ClassBasedCompDemo />
-      <hr />
-      <xyz /> */}
+    <div>
+      <p>Notes App</p>
     </div>
   );
 }
 
 export default App;
-
-
-// Class -> render function() = return JSX
-// Functional - returns JSX (JavaScript XML) -> converts into JavaScript
-// - npm install bootstrap@4 --save
---
+++ react-demo/notes-app/src/OldApp.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+// import ClassBasedCompDemo from './components/Types/ClassBasedCompDemo';
+
+// import the named function
+// import {FunctionBasedCompDemo, a, b} from "./components/Types/FunctionBasedCompDemo";
+
+// import the default function
+// import xyz, {a} from "./components/Types/FunctionBasedCompDemo";
+// import Parent from './components/CmpCommunication/Parent';
+
+// import Parent from './components/StateDemo/Parent';
+
+function App() {
+  return (
+
+    <div>
+      <p>Notes App</p>
+    </div>
+
+
+    // React.createElement("div",{className : "App"}, 
+    //   React.createElement("p",{}, "Hello World again!"))
+    // <div className="App">
+    //   <p className="display-4"> Hello World!</p>
+
+    //   <Parent />
+
+    //   {/* <hr />
+    //   <ClassBasedCompDemo />
+    //   <hr />
+    //   <xyz /> */}
+    // </div>
+  );
+}
+
+export default App;
+
+
+// Class -> render function() = return JSX
+// Functional - returns JSX (JavaScript XML) -> converts into JavaScript
+// - npm install bootstrap@4 --save
