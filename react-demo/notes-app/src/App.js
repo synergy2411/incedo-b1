b7860fce47e267cd6aebea8c2daeabe84b189fb0
unknown <Sumit Khandelwal>
2021-02-17 13:09:45 +0530

    routing added

--- react-demo/notes-app/src/App.js
+++ react-demo/notes-app/src/App.js
@@ -4,17 +4,39 @@ import React from 'react';
 import Notes from './components/Notes/Notes';
 import RefsDemo from './components/RefsDemo/RefsDemo';
 import HooksDemo from './components/HooksDemo/HooksDemo';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="container">
+      <Router>
+        <ul>
+          <li>
+            <Link to="/notes">Notes App</Link>
+          </li>
+          <li>
+            <Link to="/hooks">Hooks</Link>
+          </li>
+        </ul>
 
-    {/* <RefsDemo /> */}
-    <HooksDemo />
+        <Route path="/notes">
+          <Notes />
+        </Route>
+
+        <Route path = "/hooks">
+          <HooksDemo />
+        </Route>
+        
+      </Router>
+
+
+
+      {/* <RefsDemo /> */}
+      {/* <HooksDemo />
     <br />
 
       <p className="display-4 text-center">Notes App</p>
-      <Notes />
+      <Notes /> */}
     </div>
   );
 }
