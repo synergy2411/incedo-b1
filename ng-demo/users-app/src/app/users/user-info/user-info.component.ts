9885cb5327919987e7f3c6bb64e52d459b494360
unknown <Sumit Khandelwal>
2021-02-23 10:09:56 +0530

    attribute directive added

--- ng-demo/users-app/src/app/users/user-info/user-info.component.ts
+++ ng-demo/users-app/src/app/users/user-info/user-info.component.ts
@@ -10,4 +10,12 @@ export class UserInfoComponent {
   
 @Input() user : User;
 
+myDynamicClasses = {'my-border' : true, 'my-feature' : false}
+
+onToggle(){
+  this.myDynamicClasses['my-border'] = !this.myDynamicClasses['my-border'];
+  this.myDynamicClasses['my-feature'] = !this.myDynamicClasses['my-feature'];
+}
+
+
 }
